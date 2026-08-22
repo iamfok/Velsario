5ba1fb2eb6ac0b8396de1452cb9555d78da53264
Fokrul Islam <briefboxdigital@gmail.com>
2026-08-23 01:50:00 +0600

    layout.tsx

--- app/main/layout.tsx
+++ app/main/layout.tsx
@@ -2,6 +2,7 @@ import Navbar from '@/components/layout/Navbar'
 import Footer from '@/components/layout/Footer'
 import GlobalPageHero from '@/components/content/GlobalPageHero'
 
+
 export default function MainLayout({
   children,
 }: {
