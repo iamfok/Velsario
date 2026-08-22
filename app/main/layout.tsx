f326d1c6544947cfd7d7ee56efb8c04bdb661484
Fokrul Islam <briefboxdigital@gmail.com>
2026-08-23 01:30:47 +0600

    layout.tsx

--- app/main/layout.tsx
+++ app/main/layout.tsx
@@ -1,5 +1,6 @@
 import Navbar from '@/components/layout/Navbar'
 import Footer from '@/components/layout/Footer'
+import GlobalPageHero from '@/components/content/GlobalPageHero'
 
 export default function MainLayout({
   children,
@@ -9,9 +10,13 @@ export default function MainLayout({
   return (
     <>
       <Navbar />
+
+      <GlobalPageHero />
+
       <main className="min-h-screen">
         {children}
       </main>
+
       <Footer />
     </>
   )
